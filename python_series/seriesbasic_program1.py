803a6eb27082658e97654e5e439fedfa787824da
Sam261014 <154089537+Sam261014@users.noreply.github.com>
2024-11-30 00:02:07 +0200

    Add files via upload

---
+++ python_series/seriesbasic_program1.py
@@ -0,0 +1,4 @@
+print("This program will create sequence using sequence using series \n\n")
+import pandas as pd
+ser=pd.Series([4.5, 10, 0.5])
+print(ser)
---
+++ python_series/seriesbasic_program2.py
@@ -0,0 +1,4 @@
+print("This program will create dictionary using series \n\n")
+import pandas as pd
+dic=pd.Series({'Jan':31, 'Feb':28, 'March':31})
+print(dic)
